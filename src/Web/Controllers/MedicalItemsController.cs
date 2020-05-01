55c761df968527c44af3bd2d3061a51eda4f0d23
Dmitriy Lopatiev <dmitriy.lopatiev@gmail.com>
2020-05-01 22:49:24 +0300

    Create CRUD Actions for Medical Items.

--- src/Web/Controllers/MedicalItemsController.cs
+++ src/Web/Controllers/MedicalItemsController.cs
@@ -6,6 +6,7 @@ using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis;
 using Microsoft.EntityFrameworkCore;
 using PharmacyNetwork.ApplicationCore.Constants;
 using PharmacyNetwork.ApplicationCore.Entities;
@@ -41,131 +42,100 @@ namespace PharmacyNetwork.Web.Controllers
         // GET: MedicalItems/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null) 
-                return NotFound();
+            if (id == null) return NotFound();
 
             var medicalItem = await _repository.GetByIdAsync(id);
-            if (medicalItem == null) 
-                return NotFound();
+            if (medicalItem == null) return NotFound();
 
             return View(medicalItem);
         }
 
-        [Authorize(Roles = AuthorizationConstants.Roles.ADMINSTRATORS)]
         // GET: MedicalItems/Create
+        [Authorize(Roles = AuthorizationConstants.Roles.ADMINSTRATORS)]
         public async Task<IActionResult> Create()
         {
             var medicalItemViewModel = await Mediator.Send(new GetMedicalItem());
             return View(medicalItemViewModel);
         }
 
-        //// POST: MedicalItems/Create
-        //// To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        //// more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Create([Bind("MedItemId,FirmId,CategId,MedItemName,MedItemDescrip,MedItemPrice,MedItemPriceMarkup")] MedicalItem medicalItem)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        _context.Add(medicalItem);
-        //        await _context.SaveChangesAsync();
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    ViewData["CategId"] = new SelectList(_context.ProductCategory, "CategId", "CategName", medicalItem.CategId);
-        //    ViewData["FirmId"] = new SelectList(_context.Firm, "FirmId", "FirmAddress", medicalItem.FirmId);
-        //    return View(medicalItem);
-        //}
+        // POST: MedicalItems/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = AuthorizationConstants.Roles.ADMINSTRATORS)]
+        public async Task<IActionResult> Create(MedicalItemViewModel medicalItemViewModel)
+        {
+            if (!ModelState.IsValid) return View(medicalItemViewModel);
 
-        //// GET: MedicalItems/Edit/5
-        //public async Task<IActionResult> Edit(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
+            await _repository.AddAsync(medicalItemViewModel.MedicalItem);
+            return RedirectToAction(nameof(Index));
+        }
 
-        //    var medicalItem = await _context.MedicalItem.FindAsync(id);
-        //    if (medicalItem == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    ViewData["CategId"] = new SelectList(_context.ProductCategory, "CategId", "CategName", medicalItem.CategId);
-        //    ViewData["FirmId"] = new SelectList(_context.Firm, "FirmId", "FirmAddress", medicalItem.FirmId);
-        //    return View(medicalItem);
-        //}
+        // GET: MedicalItems/Edit/5
+        [Authorize(Roles = AuthorizationConstants.Roles.ADMINSTRATORS)]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
 
-        //// POST: MedicalItems/Edit/5
-        //// To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        //// more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit(int id, [Bind("MedItemId,FirmId,CategId,MedItemName,MedItemDescrip,MedItemPrice,MedItemPriceMarkup")] MedicalItem medicalItem)
-        //{
-        //    if (id != medicalItem.MedItemId)
-        //    {
-        //        return NotFound();
-        //    }
+            var medicalItemViewModel = await Mediator.Send(new GetMedicalItem(id));
+            if (medicalItemViewModel == null) return NotFound();
 
-        //    if (ModelState.IsValid)
-        //    {
-        //        try
-        //        {
-        //            _context.Update(medicalItem);
-        //            await _context.SaveChangesAsync();
-        //        }
-        //        catch (DbUpdateConcurrencyException)
-        //        {
-        //            if (!MedicalItemExists(medicalItem.MedItemId))
-        //            {
-        //                return NotFound();
-        //            }
-        //            else
-        //            {
-        //                throw;
-        //            }
-        //        }
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    ViewData["CategId"] = new SelectList(_context.ProductCategory, "CategId", "CategName", medicalItem.CategId);
-        //    ViewData["FirmId"] = new SelectList(_context.Firm, "FirmId", "FirmAddress", medicalItem.FirmId);
-        //    return View(medicalItem);
-        //}
+            return View(medicalItemViewModel);
+        }
 
-        //// GET: MedicalItems/Delete/5
-        //public async Task<IActionResult> Delete(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
+        // POST: MedicalItems/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = AuthorizationConstants.Roles.ADMINSTRATORS)]
+        public async Task<IActionResult> Edit(MedicalItemViewModel medicalItemViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _repository.UpdateAsync(medicalItemViewModel.MedicalItem);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MedicalItemExists(medicalItemViewModel.MedicalItem.MedItemId))
+                    {
+                        return NotFound();
+                    }
+                    throw;
+                }
 
-        //    var medicalItem = await _context.MedicalItem
-        //        .Include(m => m.Categ)
-        //        .Include(m => m.Firm)
-        //        .FirstOrDefaultAsync(m => m.MedItemId =   = id);
-        //    if (medicalItem == null)
-        //    {
-        //        return NotFound();
-        //    }
+                return RedirectToAction(nameof(Index));
+            }
 
-        //    return View(medicalItem);
-        //}
+            return View(medicalItemViewModel);
+        }
 
-        //// POST: MedicalItems/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> DeleteConfirmed(int id)
-        //{
-        //    var medicalItem = await _context.MedicalItem.FindAsync(id);
-        //    _context.MedicalItem.Remove(medicalItem);
-        //    await _context.SaveChangesAsync();
-        //    return RedirectToAction(nameof(Index));
-        //}
+        // GET: MedicalItems/Delete/5
+        [Authorize(Roles = AuthorizationConstants.Roles.ADMINSTRATORS)]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
 
-        //private bool MedicalItemExists(int id)
-        //{
-        //    return _context.MedicalItem.Any(e => e.MedItemId == id);
-        //}
+            var medicalItem = await _repository.GetByIdAsync(id);
+            if (medicalItem == null) return NotFound();
+
+            return View(medicalItem);
+        }
+
+        // POST: MedicalItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = AuthorizationConstants.Roles.ADMINSTRATORS)]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var medicalItem = await _repository.GetByIdAsync(id);
+            await _repository.DeleteAsync(medicalItem);
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MedicalItemExists(int id)
+        {
+            var list = _repository.GetAllAsync().Result;
+            return list.Any(x => x.MedItemId == id);
+        }
     }
 }
